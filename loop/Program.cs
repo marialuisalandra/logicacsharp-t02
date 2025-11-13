e7fcef58e68fed0f2bbcafb9b61b6d7dcbe5de6a
MARIA LUISA LANDRA <maria.llandra1@senacsp.edu.br>
2025-11-13 16:38:53 -0300

    loop

---
+++ loop/Program.cs
@@ -0,0 +1,76 @@
+﻿//loop for
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+int soma  = 0;
+for (int i = 1;  i <= 100; i++)
+{
+    soma += i; //Soma o valor de i à variável soma, exemplo: 1+2+3+4...+99+100
+}
+
+Console.WriteLine("A soma dos números de 1 a 100 é: " + soma);
+
+//Loop infinito
+/*
+for (;;)
+{
+    Console.WriteLine("Este é um loop infinito!");
+}
+*/
+
+
+//Loop While (enquanto)
+
+int contador = 1;
+
+while(contador <= 5)
+{
+    Console.WriteLine(contador);
+    contador++;  //incrementa o contador
+}
+
+
+
+//Loop do while (faça enquanto) 
+int novocontador = 2;
+
+do
+{
+    Console.WriteLine(novocontador);
+    novocontador++; //incrementa o contador
+} while (novocontador <= 5);
+
+
+//Loop infinito while
+/*
+while (true)
+{
+    Console.WriteLine("Este é um loop infinito!");
+}
+*/
+
+
+//Laço foreach
+
+//Uma lista de strings
+List<string> nomes = new List<string> { "Ana", "Carlos", "João", "Maria" };
+
+//Usando o foreach para percorrer a lista
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome); //imprime cada nome exibindo na tela 
+}
+
+
+//Um vetor de números inteiros 
+int[] numeros = { 1, 2, 3, 4, 5 };
+
+//Usando o foreach para percorrer o vetor 
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero); //imprime cada número exibido na tela
+}
+
---
+++ maratonaexercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 1. Saudação com Nome
+Objetivo: Declarar variáveis, usar concatenação e interpolação.
+Descrição: Crie um script que declare uma variável para o nome de 
+uma pessoa e exiba uma saudação personalizada, por exemplo: "Olá, 
+João! Bem-vindo ao nosso site!".
+Dica: Utilize tanto a concatenação (.) quanto a interpolação dentro de 
+uma string com aspas duplas.*/
+
+string nome = "João";
+Console.WriteLine($"Olá {nome}! Bem-vindo ao nosso site!");
+Console.WriteLine("Olá " + nome + "! Bem-vindo ao nosso site!");
+
+/*2. Conversão de Temperatura (Celsius para Fahrenheit)
+Objetivo: Trabalhar com cálculos matemáticos e variáveis.
+Descrição: Crie um script que converta uma temperatura em Celsius 
+para Fahrenheit. A fórmula para conversão é: 
+Dica: Declare uma constante para armazenar o valor 9/5 e faça o 
+cálculo com a variável fornecida. */
+
+float celsius = 45f;
+Console.WriteLine("Digite a temperatura em Celsius");
+
+
+
